6baad11c45e4ce6f0a31cc74b928fbc1e3f10273
ebenezerz10 <57261976+ebenezerz10@users.noreply.github.com>
2020-09-17 17:45:46 -0500

    Update url_retrieve.py

--- url_retrieve.py
+++ url_retrieve.py
@@ -4,7 +4,3 @@ URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
 
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
-
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True))
-
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
