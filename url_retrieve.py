28644253d1a098ac2ab7b13d70e83bd58bca5eee
ebenezerz10 <57261976+ebenezerz10@users.noreply.github.com>
2020-09-18 13:55:42 -0500

    new_func

--- url_retrieve.py
+++ url_retrieve.py
@@ -1,6 +1,8 @@
 from urllib.request import urlretrieve
 
 dates = []
+year_amount = []
+pure_num = []
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 local = 'local_copy.txt'
 
@@ -15,5 +17,13 @@ for row in open_log:
 print(dates)
 open_log.read(64) 
 open_log.readline() 
+
+for date in dates:
+	pure_num.append(date[1:12])
+
+if pure_num[8:12] == '1995':
+    print(len(pure_num))
+    
+    
 
 print("Total request made in last year:", len(dates))
