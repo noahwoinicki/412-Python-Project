525a9f8495479dfc06b4cb7b9fec1a79d4f77b8d
ebenezerz10 <57261976+ebenezerz10@users.noreply.github.com>
2020-09-25 15:41:33 -0500

    Update url_retrieve.py

--- url_retrieve.py
+++ url_retrieve.py
@@ -2,13 +2,16 @@ from urllib.request import urlretrieve
 from os import path
 
 dates = []
+n_dates = []
+n1_dates = []
 year_amount = []
-pure_num = []
+n_month = {}
+
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 local = 'local_copy.txt'
 
 
-if path.isfile('local_copy.txt') == False"
+if path.isfile('local_copy.txt') == "False":
     print('File downloading...')
     url_ret = urlretrieve(url, local)
     print('File Download Complete')
@@ -18,13 +21,27 @@ else:
 
 open_log = open('local_copy.txt', 'r')
 
-for row in open_log: 
-    split = row.split(' ')
-    dates.append(split[3]) 
+for date in dates:
+    n_dates.append(date[1:12])
+    n1_dates.append(date[1:3])
 
-print(dates)
-open_log.read(64) 
-open_log.readline() 
+for d in n1_dates:
+	if(day in n_month):
+		n_month[day] += 1
+	else:
+		n_month[day] = 1
+
+print("Day: Number of Requests")
+for key, value in sorted(DaysOfMonth.items()):
+	print(f"{key} : {value}")
+#####print()
+#####for row in open_log: 
+#####    split = row.split(' ')
+#####    dates.append(split[3]) 
+
+#####print(dates)
+#####open_log.read(64) 
+#####open_log.readline() 
     
     
-print("Total request made in last year:", len(dates))
+#####print("Total request made in last year:", len(dates))
