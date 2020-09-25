f130b204e12a3095a5231aa4e56e8404d89709cb
noahwoinicki <63131233+noahwoinicki@users.noreply.github.com>
2020-09-25 15:18:41 -0500

    Update url_retrieve.py

--- url_retrieve.py
+++ url_retrieve.py
@@ -1,4 +1,5 @@
 from urllib.request import urlretrieve
+from os import path
 
 dates = []
 year_amount = []
@@ -6,7 +7,14 @@ pure_num = []
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 local = 'local_copy.txt'
 
-url_ret = urlretrieve(url, local)
+
+if path.isfile('local_copy.txt') == False"
+    print('File downloading...')
+    url_ret = urlretrieve(url, local)
+    print('File Download Complete')
+else: 
+    print('File is already downloaded')
+    
 
 open_log = open('local_copy.txt', 'r')
 
