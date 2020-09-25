b765533338bec27c3523ac98b0809da3a6800a41
noahwoinicki <63131233+noahwoinicki@users.noreply.github.com>
2020-09-25 15:48:46 -0500

    Update url_retrieve.py

--- url_retrieve.py
+++ url_retrieve.py
@@ -1,16 +1,19 @@
+#preparing python
 from urllib.request import urlretrieve
 from os import path
 
+
+#variables
+url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+local = 'local_copy.txt'
 dates = []
 n_dates = []
 n1_dates = []
 year_amount = []
-n_month = {}
-
-url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-local = 'local_copy.txt'
+n_month = []
 
 
+#pull marketing info and make log 
 if path.isfile('local_copy.txt') == "False":
     print('File downloading...')
     url_ret = urlretrieve(url, local)
@@ -19,7 +22,16 @@ else:
     print('File is already downloaded')
     
 
+#open and get to work
 open_log = open('local_copy.txt', 'r')
+
+for row in open_log: 
+    split = row.split(' ')
+    if(len(split) > 8):
+        error_codes.append(split[8])
+        file_names.append(split[6])
+    if(len(split[3]) > 14):
+        dates.append(split[3]) 
 
 for date in dates:
     n_dates.append(date[1:12])
@@ -34,6 +46,7 @@ for d in n1_dates:
 print("Day: Number of Requests")
 for key, value in sorted(DaysOfMonth.items()):
 	print(f"{key} : {value}")
+	
 #####print()
 #####for row in open_log: 
 #####    split = row.split(' ')
